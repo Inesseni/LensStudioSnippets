6178f768047b3a9a01f90d18cd02839de781ba19
Ines <60435099+Inesseni@users.noreply.github.com>
2022-11-17 10:32:43 +0100

    Cleaned up a bit

--- Script_schnipsel.js
+++ Script_schnipsel.js
@@ -21,7 +21,7 @@ https://docs.snap.com/lens-studio/references/guides/lens-features/adding-interac
 
 //touch blocking disables all snapchat native touch/tap interactions like doube tap to switch camera, swipe to get to other Tab in the app itself
 global.touchSystem.touchBlocking = true;
-//you can male exceptions to still have some of the snapchat native interactions 
+//you can make exceptions to still have some of the snapchat native interactions 
 global.touchSystem.enableTouchBlockingException("TouchTypeSwipe", true);
 
 
@@ -32,16 +32,21 @@ global.behaviorSystem.addCustomTriggerResponse("my_custom_Trigger", my_function)
 //sends a custom trigger to behaviour scripts in inspektor
 global.behaviorSystem.sendCustomTrigger("my_custom_trigger");
 
-//this makes the function global and visible for other components (behaviour scripts)to call it
+    
+//this exposes the function to other scripts like behaviour or custom
 script.api.nameOfMyFunction = function () { }
 
+//call a script.api function like this:
+myScriptReference.api.nameOfMyFunction();
 
-//call any global function
-global.myGlobalFunction();
-//Any global function in different script:
+
+//Makes this function glbally accessable, no reference to the script needed
 global.myGlobalFunction = function () {
     print("im here");
 }
+    
+//call global functions from anywhere like this:
+global.myGlobalFunction();
 
 
 
@@ -58,10 +63,10 @@ Update_event.bind(function (eventData) {
 // Bind a function to the tap event.
 var touchEvent = script.createEvent("TapEvent");
 touchEvent.bind(my_function_name_here);
-//Other event types: https://lensstudio.snapchat.com/api/Events/
-//zb: 
-// SnapImageCaptureEvent , SnapRecordStartEvent , SmileStartedEvent , CameraBackEvent , CameraFrontEvent
 
+// other event examples: 
+// SnapImageCaptureEvent , SnapRecordStartEvent , SmileStartedEvent , CameraBackEvent , CameraFrontEvent
+// Get the full event list here: https://lensstudio.snapchat.com/api/Events
 
 
 
@@ -69,7 +74,7 @@ touchEvent.bind(my_function_name_here);
 //Delay function
 var delayedEvent = script.createEvent("DelayedCallbackEvent");
 delayedEvent.bind(function (eventData) {
-    //Do stuff here // call other functions etc 
+    //Do stuff here 
 });
 delayedEvent.reset(2);  //amount of seconds to delay
 
@@ -111,8 +116,7 @@ setMaterial(script.materials[0]); //set material based on index of Materials lis
 
 
 
-
-//Play AnimationMixer
+// Play AnimationMixer
 var animationWeight = 1;
 var animationStartOffset = 0;
 var numberOfLoops = 1;  // -1 would be infinite loops
@@ -122,8 +126,7 @@ myAnimationMixer.start("animationLayerName", animationStartOffset, numberOfLoops
 
 
 
-
-//Animated texture controlls
+// Animated texture controlls
 //@input Component.Image 
 script.Image.mainMaterial.mainPass.baseTex.control.play(-1, 0);
 script.Image.mainMaterial.mainPass.baseTex.control.stop();
@@ -131,13 +134,14 @@ var animationDuration = script.Image.mainMaterial.mainPass.baseTex.control.durat
 
 
 
-//Get a component of a scene Object (here a Image)
+//Get a component of a scene Object (here an Image)
 script.objectWithImage.getComponent("Component.Image");
 
 
 
 
 //Sets a new texture
+//@input Asset.Texture myTexture
 script.myImage.mainPass.baseTex = script.myTexture;
 
 
@@ -145,7 +149,6 @@ script.myImage.mainPass.baseTex = script.myTexture;
 
 //Checks camera type
 if (global.scene.getCameraType() == "back") { }// oder "front"
-
 
 
 
@@ -180,8 +183,10 @@ var MyScneObject_Child = script.MySceneObj.getChild(0);
 
 
 //gets a random number between 1 and 10 - You can change the numbers
-var randomINT = Math.floor(Math.random() * 10) + 1;
-var randomFLOAT = Math.random() * (10 - 1) + 1;
+var min = 1;
+var max = 10;
+var randomINT = Math.floor(Math.random() * max) + min;
+var randomFLOAT = Math.random() * (max - min) + min;
 
 //random float between -3 and 3
 var rangeX = 3;
@@ -203,7 +208,7 @@ script.Particle_material.mainPass.spawnMaxParticles = 200;
 
 
 
-//show Lens studio native Hint (wird übersetzt in jeweilige sprache) 
+//show Lens studio native Hint (automatically translated in the device language) 
 //@input string hintId {"widget":"combobox","values":[{"label":"Raise your hand","value":"lens_hint_show_your_hand"},{"label":"Aim camera at the sky","value":"lens_hint_aim_camera_at_the_sky"},{"label":"Blow a kiss","value":"lens_hint_blow_a_kiss"},{"label":"Blow a kiss voice changer","value":"lens_hint_blow_a_kiss_voice_changer"},{"label":"Come closer","value":"lens_hint_come_closer"},{"label":"Do not smile","value":"lens_hint_do_not_smile"},{"label":"Do not try with a friend","value":"lens_hint_do_not_try_with_a_friend"},{"label":"Draw with your finger","value":"lens_hint_draw_with_your_finger"},{"label":"Find face","value":"lens_hint_find_face"},{"label":"Find image","value":"lens_hint_find_image"},{"label":"Find marker","value":"lens_hint_find_marker"},{"label":"Find snapcode","value":"lens_hint_find_snapcode"},{"label":"Kiss","value":"lens_hint_kiss"},{"label":"Kiss again","value":"lens_hint_kiss_again"},{"label":"Look around","value":"lens_hint_look_around"},{"label":"Look down","value":"lens_hint_look_down"},{"label":"Look left","value":"lens_hint_look_left"},{"label":"Look right","value":"lens_hint_look_right"},{"label":"Look up","value":"lens_hint_look_up"},{"label":"Make some noise!","value":"lens_hint_make_some_noise"},{"label":"Move your head","value":"lens_hint_move_your_head"},{"label":"Nod your head","value":"lens_hint_nod_your_head"},{"label":"Now kiss","value":"lens_hint_now_kiss"},{"label":"Now open your mouth","value":"lens_hint_now_open_your_mouth"},{"label":"Now raise your eyebrows","value":"lens_hint_now_raise_your_eyebrows"},{"label":"Now smile","value":"lens_hint_now_smile"},{"label":"Open your mouth","value":"lens_hint_open_your_mouth"},{"label":"Open your mouth again","value":"lens_hint_open_your_mouth_again"},{"label":"Open your mouth voice changer","value":"lens_hint_open_your_mouth_voice_changer"},{"label":"Pick a face","value":"lens_hint_pick_a_face"},{"label":"Pick a photo","value":"lens_hint_pick_a_photo"},{"label":"Pick an image","value":"lens_hint_pick_an_image"},{"label":"Raise your eyebrows","value":"lens_hint_raise_your_eyebrows"},{"label":"Raise your eyebrows again","value":"lens_hint_raise_your_eyebrows_again"},{"label":"Raise your eyebrows or open your mouth","value":"lens_hint_raise_eyebrows_or_open_mouth"},{"label":"Raise your eyebrows voice changer","value":"lens_hint_raise_your_eyebrows_voice_changer"},{"label":"Rotate your phone","value":"lens_hint_rotate_your_phone"},{"label":"Say something","value":"lens_hint_say_something"},{"label":"Smile","value":"lens_hint_smile"},{"label":"Smile again","value":"lens_hint_smile_again"},{"label":"Smile voice changer","value":"lens_hint_smile_voice_changer"},{"label":"Swap camera","value":"lens_hint_swap_camera"},{"label":"Tap a surface","value":"lens_hint_tap_a_surface"},{"label":"Tap ground to place","value":"lens_hint_tap_ground_to_place"},{"label":"Tap surface to place","value":"lens_hint_tap_surface_to_place"},{"label":"Tap the ground","value":"lens_hint_tap_ground"},{"label":"Tap!","value":"lens_hint_tap"},{"label":"Tilt your head","value":"lens_hint_tilt_your_head"},{"label":"Try it with a friend","value":"lens_hint_try_friend"},{"label":"Try it with your rear camera","value":"lens_hint_try_rear_camera"},{"label":"Turn around","value":"lens_hint_turn_around"},{"label":"Voice changer","value":"lens_hint_voice_changer"},{"label":"Walk through the door","value":"lens_hint_walk_through_the_door"}]}
 
 if (!script.initialized) {
@@ -243,19 +248,19 @@ var distance = CalcDistance(Point1, Point2);
 
 
 
-
-//Convert QUAT(vec4) to EULER(vec3) (to use in a setRotation())
-var rotationEuler = quat.fromEulerVec(myVec3);
-
 //Create Euler from Values :
 fromEulerAngles(Number_x, Number_y, Number_z);
+    
+//Convert EULER(vec3) to QUAT(vec4) (to use in a setRotation() for example)
+var rotationEuler = quat.fromEulerVec(myVec3);
+
 
 //Convert QUAT to EULER(vec3):
 var rotationQuat = rotationEuler.toEulerAngles();
 
 
 
-//Convert hexValues to RGBA
+//Convert HEX color codes to RGBA colors
 function hexToRgbA(hex) {
     var c;
     if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
@@ -269,7 +274,8 @@ function hexToRgbA(hex) {
     }
     throw new Error('Bad Hex');
 }
-hexToRgbA('#fbafff')
+//Pass a Hex code into the function to get their RGBA value
+var myNewRGBA = hexToRgbA('#fbafff');
 
 
 
@@ -285,7 +291,7 @@ if (a >= 8) {
 
 
 
-//Swipe direction:
+//Swipe direction (THIS ALSO EXISTS IN THE ASSET LIBRARY :)
 var touchStartPos;
 
 var event = script.createEvent("TouchStartEvent");
